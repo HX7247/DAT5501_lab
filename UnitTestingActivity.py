0a3c6e72287b240e1a47b3bae10574fd99adb393
HX7247 <HX7247>
2025-09-25 14:29:31 +0100

    example1

--- UnitTestingActivity.py
+++ UnitTestingActivity.py
@@ -3,4 +3,18 @@ import unittest
 def add(a, b):
     return a + b
 
+class TestFunction(unittest.TestCase):
+    def test_positive_addition(self):
+        result = add(39, 12)
+        self.assertEqual(result, 51)
 
+    def test_negative_addition(self):
+        result = add(-8, -11)
+        self.assertEqual(result, -19)
+
+    def test_decimal_addition(self):
+        result = add(3.5, 5.5)
+        self.assertAlmostEqual(result, 9.0, places=2)
+
+if __name__ == "__main__":
+    unittest.main()
