ee05efad483dacab31629d4c57f0b203421c92cc
HX7247 <HX7247>
2025-09-29 10:50:52 +0100

    commit2

--- InterestRateCalculator.py
+++ InterestRateCalculator.py
@@ -3,7 +3,8 @@ def CompoundInterestCalculator(savings, annualInterestRate, years ):
     multiply = annualInterestRate/100
     for i in range (years):
         savings *= 1 + multiply
-        text = "Your savings after " + str(i+1) + " years is " + str(savings)
+        text1 = "Your savings after " + str(i+1) + " years is " + str(savings)
+        print(text1)
    
     number_of_years_to_double = 72/multiply
 
